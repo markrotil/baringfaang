8fc949c355b0e801842a51fbc33f9d9e54c813c5
markrotil <mark.rotil@hotmail.com>
2021-03-23 01:47:53 -0400

    push my app

---
+++ app.py
@@ -0,0 +1,199 @@
+from flask import Flask, render_template, jsonify, request, redirect, url_for
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine
+
+engine = create_engine(
+    'postgres://qzewnmvp:KLrEMMwqNGoGYgkvTiI5pNANap7CuFbP@ziggy.db.elephantsql.com:5432/qzewnmvp', echo=False)
+
+Base = automap_base()
+Base.prepare(engine, reflect=True)
+
+stocks = Base.classes.stocks
+employees = Base.classes.employees
+offices = Base.classes.offices
+
+
+session = Session(engine)
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+@app.route("/map")
+def map():
+    return render_template("map.html")
+
+
+@app.route("/")
+def fbprophet():
+    return render_template("map.html")
+
+
+@app.route("/api/stocks")
+def stockage():
+
+    amazon_data = session.query(stocks.Date, stocks.Adj_Close, stocks.Ticker).filter(
+        stocks.Ticker == 'AMZN').all()
+
+    amazon_Date = []
+    for item in amazon_data:
+        amazon_Date.append(item[0])
+
+    amazon_Close = []
+    for item in amazon_data:
+        amazon_Close.append(item[1])
+
+    amazon_Ticker = []
+    for item in amazon_data:
+        amazon_Ticker.append(item[2])
+
+    amazon_output = {"amazon_Date": amazon_Date,
+                     "amazon_Close": amazon_Close,
+                     "amazon_Volume": amazon_Ticker
+                     }
+
+    netflix_data = session.query(stocks.Date, stocks.Adj_Close, stocks.Ticker).filter(
+        stocks.Ticker == 'NFLX').all()
+
+    netflix_Date = []
+    for item in netflix_data:
+        netflix_Date.append(item[0])
+
+    netflix_Close = []
+    for item in netflix_data:
+        netflix_Close.append(item[1])
+
+    netflix_Ticker = []
+    for item in netflix_data:
+        netflix_Ticker.append(item[2])
+
+    netflix_output = {"netflix_Date": netflix_Date,
+                      "netflix_Close": netflix_Close,
+                      "netflix_Volume": netflix_Ticker
+                      }
+
+    apple_data = session.query(stocks.Date, stocks.Adj_Close, stocks.Ticker).filter(
+        stocks.Ticker == 'AAPL').all()
+
+    apple_Date = []
+    for item in apple_data:
+        apple_Date.append(item[0])
+
+    apple_Close = []
+    for item in apple_data:
+        apple_Close.append(item[1])
+
+    apple_Ticker = []
+    for item in apple_data:
+        apple_Ticker.append(item[2])
+
+    apple_output = {"apple_Date": apple_Date,
+                    "apple_Close": apple_Close,
+                    "apple_Volume": apple_Ticker
+                    }
+
+    facebook_data = session.query(stocks.Date, stocks.Adj_Close, stocks.Ticker).filter(
+        stocks.Ticker == 'FB').all()
+
+    facebook_Date = []
+    for item in facebook_data:
+        facebook_Date.append(item[0])
+
+    facebook_Close = []
+    for item in facebook_data:
+        facebook_Close.append(item[1])
+
+    facebook_Ticker = []
+    for item in facebook_data:
+        facebook_Ticker.append(item[2])
+
+    facebook_output = {"facebook_Date": facebook_Date,
+                       "facebook_Close": facebook_Close,
+                       "facebook_Volume": facebook_Ticker
+                       }
+
+    google_data = session.query(stocks.Date, stocks.Adj_Close, stocks.Ticker).filter(
+        stocks.Ticker == 'GOOG').all()
+
+    google_Date = []
+    for item in google_data:
+        google_Date.append(item[0])
+
+    google_Close = []
+    for item in google_data:
+        google_Close.append(item[1])
+
+    google_Ticker = []
+    for item in google_data:
+        google_Ticker.append(item[2])
+
+    google_output = {"google_Date": google_Date,
+                     "google_Close": google_Close,
+                     "google_Volume": google_Ticker
+                     }
+
+    microsoft_data = session.query(stocks.Date, stocks.Adj_Close, stocks.Ticker).filter(
+        stocks.Ticker == 'MSFT').all()
+
+    microsoft_Date = []
+    for item in microsoft_data:
+        microsoft_Date.append(item[0])
+
+    microsoft_Close = []
+    for item in microsoft_data:
+        microsoft_Close.append(item[1])
+
+    microsoft_Ticker = []
+    for item in microsoft_data:
+        microsoft_Ticker.append(item[2])
+
+    microsoft_output = {"microsoft_Date": microsoft_Date,
+                        "microsoft_Close": microsoft_Close,
+                        "microsoft_Volume": microsoft_Ticker
+                        }
+
+    stocks_list = []
+    stocks_list.extend((amazon_output, netflix_output,
+                        apple_output, facebook_output, google_output, microsoft_output))
+    return jsonify(stocks_list)
+
+
+@app.route("/employees")
+def employee_():
+
+    employee = session.query(
+        employees.Company, employees.Number_of_Employees, employees.Year).all()
+
+    emp_list = []
+    for row in employee:
+        emp_dict = {"Company": row[0],
+                    "Number_of_Employees": row[1],  "Year": row[2]}
+        emp_list.append(emp_dict)
+    session.close()
+    return jsonify(emp_list)
+
+
+@app.route("/office")
+def location():
+
+    offices_ = session.query(
+        offices.Company, offices.Country, offices.Office,
+        offices.Latitude, offices.Longitude).all()
+
+    off_list = []
+    for row in offices_:
+        off_dict = {"Company": row[0], "Country": row[1], "Office": row[2], "Latitude": row[3],
+                    "Longitude": row[4]}
+        off_list.append(off_dict)
+    session.close()
+    return jsonify(off_list)
+
+
+if __name__ == "__main__":
+    app.run()
---
+++ static/js/map.js
@@ -0,0 +1,176 @@
+var LeafIcon = L.Icon.extend({
+    options: {
+        iconSize: [18, 18],
+        iconAnchor: [12, 74],
+        popupAnchor: [-3, -76]
+    }
+})
+
+
+var Amzn = new LeafIcon({ iconUrl: 'amazon pin.png' }),
+    Appl = new LeafIcon({ iconUrl: 'apple pin.png' }),
+    Googl = new LeafIcon({ iconUrl: 'google pin.png' }),
+    FacB = new LeafIcon({ iconUrl: 'facebook pin.png' }),
+    Nflx = new LeafIcon({ iconUrl: 'netflix pin.png' }),
+    Micrsft = new LeafIcon({ iconUrl: 'microsoft pin.png' });
+
+
+// Create the tile layer that will be the background of our map
+var lightmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+    maxZoom: 5,
+    id: "light-v10",
+    accessToken: API_KEY
+});
+
+// Initialize all of the LayerGroups we'll be using
+var layers = {
+    Amazon: new L.LayerGroup(),
+    Apple: new L.LayerGroup(),
+    Facebook: new L.LayerGroup(),
+    Google: new L.LayerGroup(),
+    Netflix: new L.LayerGroup(),
+    Microsoft: new L.LayerGroup()
+
+};
+
+// Create the map with our layers
+var map = L.map("map-id", {
+    center: [40.73, -74.0059],
+    zoom: 35,
+    layers: [
+        layers.Amazon,
+        layers.Apple,
+        layers.Facebook,
+        layers.Google,
+        layers.Netflix,
+        layers.Microsoft
+    ]
+});
+
+// Add our 'lightmap' tile layer to the map
+lightmap.addTo(map);
+
+// Create an overlays object to add to the layer control
+var overlays = {
+    "Amazon": layers.Amazon,
+    "Apple": layers.Apple,
+    "Facebook": layers.Facebook,
+    "Google": layers.Google,
+    "Netflix": layers.Netflix,
+    "Microsoft": layers.Microsoft
+};
+
+// Create a control for our layers, add our overlay layers to it
+L.control.layers(null, overlays).addTo(map);
+
+// Create a legend to display information about our map
+var info = L.control({
+    position: "bottomright"
+});
+
+// When the layer control is added, insert a div with the class of "legend"
+info.onAdd = function () {
+    var div = L.DomUtil.create("div", "legend");
+    return div;
+};
+// Add the info legend to the map
+info.addTo(map);
+
+// Initialize an object containing icons for each layer group
+var icons = {
+    Amazon: Amzn,
+
+    Apple: Appl,
+
+    Netflix: Googl,
+
+    Facebook: FacB,
+
+    Google: Nflx,
+
+    Microsoft: Micrsft
+};
+
+// Perform an API call to the Citi Bike Station Information endpoint
+d3.json("/office", function (infoRes) {
+    console.log(infoRes)
+    // When the first API call is complete, perform another call to the Citi Bike Station Status endpoint
+    d3.json("/office", function (Company) {
+        var updatedAt = infoRes.last_updated;
+        var stationStatus = Company.off_list;
+        var stationInfo = infoRes.off_list;
+
+        // Create an object to keep of the number of markers in each layer
+        var stationCount = {
+            Amazon: 0,
+            Apple: 0,
+            Facebook: 0,
+            Google: 0,
+            Netflix: 0
+        };
+
+        // Initialize a stationStatusCode, which will be used as a key to access the appropriate layers, icons, and station count for layer group
+        var stationStatusCode;
+
+        // Loop through the stations (they're the same size and have partially matching data)
+        for (var i = 0; i < stationInfo.length; i++) {
+
+            // Create a new station object with properties of both station objects
+            var station = Object.assign({}, stationInfo[i], stationStatus[i]);
+            // If a station is listed but not installed, it's coming soon
+            if (!station.Company == "Amazon") {
+                stationStatusCode = "Amazon";
+            }
+            // If a station has no bikes available, it's Apple
+            else if (!station.Company == "Apple") {
+                stationStatusCode = "Apple";
+            }
+            // If a station is installed but isn't renting, it's out of order
+            else if (!station.Company == "Facebook") {
+                stationStatusCode = "Facebook";
+            }
+            // If a station has less than 5 bikes, it's status is Facebook
+            else if (!station.Company == "Google") {
+                stationStatusCode = "Google";
+            }
+            // If a station has less than 5 bikes, it's status is Facebook
+            else if (!station.Company == "Microsoft") {
+                stationStatusCode = "Microsoft";
+            }
+            // Otherwise the station is Google
+            else {
+                stationStatusCode = "Netflix";
+            }
+
+            // Update the station count
+            stationCount[stationStatusCode]++;
+            // Create a new marker with the appropriate icon and coordinates
+            var newMarker = L.marker([station.lattitude, station.longitude], {
+                icon: icons[stationStatusCode]
+            });
+
+            // Add the new marker to the appropriate layer
+            newMarker.addTo(layers[stationStatusCode]);
+
+            // Bind a popup to the marker that will  display on click. This will be rendered as HTML
+            newMarker.bindPopup(station.Company + "<br> Office: " + station.Office + "<br>" + station.Country + " Bikes Available");
+        }
+
+        // Call the updateLegend function, which will... update the legend!
+        updateLegend(updatedAt, stationCount);
+    });
+});
+
+// Update the legend's innerHTML with the last updated time and station count
+function updateLegend(time, stationCount) {
+    document.querySelector(".legend").innerHTML = [
+        "<p>Updated: " + moment.unix(time).format("h:mm:ss A") + "</p>",
+        "<p" + stationCount.Netflix + "</p>",
+        "<p" + stationCount.Amazon + "</p>",
+        "<p" + stationCount.Apple + "</p>",
+        "<p" + stationCount.Facebook + "</p>",
+        "<p" + stationCount.Google + "</p>"
+    ].join("");
+}
+
