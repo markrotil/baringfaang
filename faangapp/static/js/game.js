525df6dd867c36d0d45d7a092a2d24f6d86f25b3
markrotil <mark.rotil@hotmail.com>
2021-03-25 19:17:02 -0400

    push

---
+++ faangapp/static/js/game.js
@@ -0,0 +1,117 @@
+
+document.addEventListener('DOMContentLoaded', () => {
+    //card options
+    const cardArray = [
+        {
+            name: 'kyla',
+            img: 'static/assets/img/matching/kyla.PNG'
+        },
+        {
+            name: 'laurel',
+            img: 'static/assets/img/matching/laurel.PNG'
+        },
+        {
+            name: 'nitin',
+            img: 'static/assets/img/matching/Nitin.PNG'
+        },
+        {
+            name: 'piro',
+            img: 'static/assets/img/matching/piro.PNG'
+        },
+        {
+            name: 'sunil',
+            img: 'static/assets/img/matching/sunil.PNG'
+        },
+        {
+            name: 'sara',
+            img: 'static/assets/img/matching/Sarah.PNG'
+        },
+        {
+            name: 'kyla',
+            img: 'static/assets/img/matching/kyla.PNG'
+        },
+        {
+            name: 'laurel',
+            img: 'static/assets/img/matching/laurel.PNG'
+        },
+        {
+            name: 'nitin',
+            img: 'static/assets/img/matching/Nitin.PNG'
+        },
+        {
+            name: 'piro',
+            img: 'static/assets/img/matching/piro.PNG'
+        },
+        {
+            name: 'sunil',
+            img: 'static/assets/img/matching/sunil.PNG'
+        },
+        {
+            name: 'sara',
+            img: 'static/assets/img/matching/Sarah.PNG'
+        },
+    ]
+
+    cardArray.sort(() => 0.5 - Math.random())
+
+    const grid = document.querySelector('.grid')
+    const resultDisplay = document.querySelector('#result')
+    let cardsChosen = []
+    let cardsChosenId = []
+    let cardsWon = []
+
+    //create your board
+    function createBoard() {
+        for (let i = 0; i < cardArray.length; i++) {
+            const card = document.createElement('img')
+            card.setAttribute('src', 'static/assets/img/matching/card back blue.png')
+            card.setAttribute('data-id', i)
+            card.addEventListener('click', flipCard)
+            grid.appendChild(card)
+        }
+    }
+
+    //check for matches
+    function checkForMatch() {
+        const cards = document.querySelectorAll('img')
+        const optionOneId = cardsChosenId[0]
+        const optionTwoId = cardsChosenId[1]
+
+        if (optionOneId == optionTwoId) {
+            cards[optionOneId].setAttribute('src', 'static/assets/img/matching/card back blue.png')
+            cards[optionTwoId].setAttribute('src', 'static/assets/img/matching/card back blue.png')
+            alert('You have clicked the same image!')
+        }
+        else if (cardsChosen[0] === cardsChosen[1]) {
+            alert('You found a match')
+            cards[optionOneId].setAttribute('src', 'static/assets/img/matching/card back green.png')
+            cards[optionTwoId].setAttribute('src', 'static/assets/img/matching/card back green.png')
+            cards[optionOneId].removeEventListener('click', flipCard)
+            cards[optionTwoId].removeEventListener('click', flipCard)
+            cardsWon.push(cardsChosen)
+        } else {
+            cards[optionOneId].setAttribute('src', 'static/assets/img/matching/card back blue.png')
+            cards[optionTwoId].setAttribute('src', 'static/assets/img/matching/card back blue.png')
+            alert('Sorry, try again')
+        }
+        cardsChosen = []
+        cardsChosenId = []
+        resultDisplay.textContent = cardsWon.length
+        if (cardsWon.length === cardArray.length / 2) {
+            resultDisplay.textContent = 'Congratulations! You found them all!'
+        }
+    }
+
+    //flip your card
+    function flipCard() {
+        let cardId = this.getAttribute('data-id')
+        cardsChosen.push(cardArray[cardId].name)
+        cardsChosenId.push(cardId)
+        this.setAttribute('src', cardArray[cardId].img)
+        if (cardsChosen.length === 2) {
+            setTimeout(checkForMatch, 500)
+        }
+    }
+
+    createBoard()
+})
